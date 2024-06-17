0fa92b1cb5c3661835a29ab6b07fef50b51ec5fd
Aniket-dev-tech <nitnawareaniket167@gmail.com>
2024-06-17 09:57:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Aniket"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string = ""
+// boolean = true,false
+// null = standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof(age));
