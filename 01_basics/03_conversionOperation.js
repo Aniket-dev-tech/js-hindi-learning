e38754ee089ae1d0858881fa51aa06581a397202
Aniket-dev-tech <nitnawareaniket167@gmail.com>
2024-06-17 19:04:27 +0000

    some operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+// "33abc" => NaN 
+// true => 1, false => 0 
+
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 => true, 0 => false 
+// "" => false, "aniket" => true
+
+let someNumber = 18
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************************Operations*********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Hello"
+let str2 = " Aniket"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2 )
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
