3f3100a3056c64477b0db1ebbe3880090f5a4b43
Aniket-dev-tech <nitnawareaniket167@gmail.com>
2024-06-17 09:58:27 +0000

    intro

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+const accountId = 8767
+let accountEmail = "nitnawareaniket7@gmail.com"
+var accountPassword = "3931"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 82424 // not allowed
+
+accountEmail = "nitnawareniket222@gmail.com"
+accountPassword = "1234"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
