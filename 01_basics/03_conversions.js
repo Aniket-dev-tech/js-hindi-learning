71ca8ad64c164ca877376274cbcdf1bad424efb7
Aniket-dev-tech <nitnawareaniket167@gmail.com>
2024-06-17 16:42:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+// "33abc" => NaN 
+// true => 1, false => 0 
+
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 => true, 0 => false 
+// "" => false, "aniket" => true
+
+let someNumber = 18
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
